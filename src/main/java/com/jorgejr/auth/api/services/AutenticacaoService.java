00cb3a3019158489473f2d78988b1d8eedcc44fd
Jorge jr <jorgesilvajunior10@gmail.com>
2024-03-14 14:01:11 -0300

    token jwt:lipstick:✅

---
+++ src/main/java/com/jorgejr/auth/api/services/AutenticacaoService.java
@@ -0,0 +1,9 @@
+package com.jorgejr.auth.api.services;
+
+import com.jorgejr.auth.api.dtos.AuthDto;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface AutenticacaoService extends UserDetailsService {
+
+     public String obterToken(AuthDto authDto);
+}
--- src/main/java/com/jorgejr/auth/api/services/impl/AutenticacaoServiceImpl.java
+++ src/main/java/com/jorgejr/auth/api/services/impl/AutenticacaoServiceImpl.java
@@ -1,20 +1,64 @@
 package com.jorgejr.auth.api.services.impl;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.jorgejr.auth.api.dtos.AuthDto;
+import com.jorgejr.auth.api.models.Usuario;
 import com.jorgejr.auth.api.repositories.UsuarioRepository;
+import com.jorgejr.auth.api.services.AutenticacaoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
 @Service
-public class AutenticacaoServiceImpl implements UserDetailsService {
+public class AutenticacaoServiceImpl implements AutenticacaoService {
 
     @Autowired
     private UsuarioRepository repository;
 
+    private AutenticacaoService autenticacaoService;
+
     @Override
     public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
         return repository.findByLogin(login);
+    }
+
+    @Override
+    public String obterToken(AuthDto authDto){
+
+        Usuario usuario = repository.findByLogin(authDto.login());
+
+        return geraTokenJwt(usuario);
+    }
+
+    public String geraTokenJwt(Usuario usuario){
+
+        try {
+
+            Algorithm algorithm = Algorithm.HMAC256("my-secret");
+
+            return JWT.create()
+                    .withIssuer("auth-api")
+                    .withSubject(usuario.getLogin())
+                    .withExpiresAt(geraDataExpiracao())
+                    .sign(algorithm);
+
+
+        } catch (JWTCreationException exception){
+             throw new RuntimeException("Erro ao tentar gerar token! " + exception.getMessage());
+        }
+    }
+
+    private Instant geraDataExpiracao(){
+          return LocalDateTime.now()
+                  .plusHours(8)
+                  .toInstant(ZoneOffset.of("-03:00"));
     }
 }
